1befcdb5f6fb10f40a1dab8403a45bb7f0d20679
sadekeen20 <udoy320@gmail.com>
2020-08-07 23:06:23 +0600

    done with comments

---
+++ true-false.js
@@ -0,0 +1,34 @@
+// console.log()
+
+const age=-4;
+if(age){
+    console.log("true");
+}
+else{
+    console.log("false")
+}
+//fact1 : 4 or -4 are considered true ; only 0 is false
+
+// console.log();
+
+
+const name=" ";
+if(name){
+    console.log("true");
+}
+else{
+    console.log("false")
+}
+//fact 2 : empty string is false but whitespace is true
+
+let fame;
+if(fame){
+    console.log("true");
+}
+else{
+    console.log("false")
+}
+//fact 3: undefined is by default false. if set null its also false . NaN case also false .
+// NaN -> not a number 
+
+//fact 4: empty array fame=[] is true . 
